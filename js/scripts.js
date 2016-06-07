e3e7933f63b48f6f61e012f05cadcd884ad9b404
Lauren Posey and Tony Fabery <lauren+tony@>
2016-06-07 13:35:51 -0700

    return string with no vowels

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 // ============= BACKEND ==============
 var antiVowel = [];
 function evalInput(arrayValue) {
-  antiVowel = arrayValue.split(" ");
+  antiVowel = arrayValue.split('');
   for (var i = 0; i < antiVowel.length; i++) {
-
+    switch(antiVowel[i]) {
+      case 'a': case 'e': case 'i': case 'o': case 'u':
+        antiVowel[i] = '-';
+    }
   }
+  return antiVowel.join('');
 }
 
 
@@ -16,7 +20,10 @@ function evalInput(arrayValue) {
 $(document).ready(function(){
   $("form#wordPuzzle").submit(function(event){
     event.preventDefault();
-    evalInput($("#word").val())
+    var noVowel = evalInput($("#word").val());
 
+    $("#noVowels").append(noVowel);
+    $("#noVowels").show();
+    $("#wordPuzzle").toggle();
   });
 });
